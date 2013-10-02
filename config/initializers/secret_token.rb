5b8810f6054cae09cec8d86d8fbda6ee7ca8ceba
Manish Dangol <manishnm@hotmail.com>
2013-10-01 21:59:36 -0400

    version 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marathonrunnerphotos::Application.config.secret_token = '65951a5fc7087cd9033791358f72cdbdb50d07a71084279d1f0719937019247d1c7c7c38c0ac4daffa84f6fd8d49ba37921ea5747ec933fa77a430e765e8347b'
