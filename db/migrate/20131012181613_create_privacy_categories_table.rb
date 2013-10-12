580976da0be321d4bc2d1cc05d118bee8d7a33d0
bandell <bandell@bandell-NV52-Series.(none)>
2013-10-12 14:56:39 -0400

    test

--- db/migrate/20131012181613_create_privacy_categories_table.rb
+++ db/migrate/20131012181613_create_privacy_categories_table.rb
@@ -1,6 +1,7 @@
 class CreatePrivacyCategoriesTable < ActiveRecord::Migration
   def change
     create_table :privacy_categories do |t|
+      #testing commit
       t.string :category
     end
   end
