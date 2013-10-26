591c5e9b6001d734fdaead1c5d328abfc86dd698
bandell <bandell@bandell-NV52-Series.(none)>
2013-10-26 14:19:42 -0400

    User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Marathonrunnerphotos::Application.routes.draw do
+  get "users/index"
+
+  get "users/login"
+
+  get "users/logout"
+
   get "index/login"
 
   get "test_branch_controller_manish/manish"
@@ -56,7 +62,7 @@ Marathonrunnerphotos::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'index#login'
 
   # See how all your routes lay out with "rake routes"
 
