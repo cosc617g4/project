db2bf24e49aaa399db8a06fab9990263ef0f4216
Manish Dangol <manishnm@hotmail.com>
2013-11-14 23:51:09 -0500

    added comments features, fixed links

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -22,6 +22,7 @@ class PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
     @comments = Comment.all
+    
 
     respond_to do |format|
       format.html # show.html.erb
