f5b2fd78cd1bd9dc739b826ce4febc2c581586f7
bradgnar <bradgnar@gmail.com>
2013-12-10 07:28:27 -0500

    changed administrator to make it work

--- app/controllers/administrator_controller.rb
+++ app/controllers/administrator_controller.rb
@@ -2,11 +2,10 @@ class AdministratorController < ApplicationController
   def index
 		if(current_user == nil)
 			redirect_to new_user_session_path
-		end
-
-		if(!current_user.admin?)
+		elsif(!current_user.admin?)
 			redirect_to root_path
-		end
+		else
+		end	
 
 	end
 
