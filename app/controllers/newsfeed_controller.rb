4d4e3cb0e4c0d465077930a3b5340ce21336f43b
bandell <bandell@bandell-NV52-Series.(none)>
2013-12-16 21:46:12 -0500

    fixed errors in newsfeed

--- app/controllers/newsfeed_controller.rb
+++ app/controllers/newsfeed_controller.rb
@@ -2,129 +2,130 @@ class NewsfeedController < ApplicationController
   def main
     if( !user_signed_in? )
       redirect_to new_user_session_path
-    end
+    else
 =begin
-require 'feedzirra'
-#get the latest database updates from their friends trainings
-@feed = Feedzirra::Feed.fetch_and_parse("http://www.runnersworld.com/feeds/runners-world-feed")
-@entries = @feed.entries
+    require 'feedzirra'
+    #get the latest database updates from their friends trainings
+    @feed = Feedzirra::Feed.fetch_and_parse("http://www.runnersworld.com/feeds/runners-world-feed")
+    @entries = @feed.entries
 =end
-    tempPosts = Array.new
+      tempPosts = Array.new
 
-    friendIDs = Array.new
+      friendIDs = Array.new
 
-    friendships = Friendship.where("user_id = ?", current_user.id)
-    friendships.each do |friendship|
-      friendIDs.push(friendship.friend_id)
-    end
-
-    #get all the marathons that friends ran
-    usermarathons = UserMarathon.where(:user_id => friendIDs)
-    usermarathons.each do |um|
-      marathon = Marathon.find(um.marathon_id)
-      runner = User.find(um.user_id)
-
-      minuteString = String.new
-      minutes = um.minutes
-      if minutes < 10
-        minuteString = "0" + minutes.to_s
-      else
-      minuteString = minutes.to_s
+      friendships = Friendship.where("user_id = ?", current_user.id)
+      friendships.each do |friendship|
+        friendIDs.push(friendship.friend_id)
       end
 
-      message = "<a href=\"/profile/index?user_id=" + runner.id.to_s + "\">" + runner.fullname + "</a> ran the " + marathon.name + " and placed " + um.place.to_s +
-      " with a time of " + um.hours.to_s + ":" + minuteString
+      #get all the marathons that friends ran
+      usermarathons = UserMarathon.where(:user_id => friendIDs)
+      usermarathons.each do |um|
+        marathon = Marathon.find(um.marathon_id)
+        runner = User.find(um.user_id)
 
-      post = Post.new
-      post.updated_at = um.updated_at
-      post.message = message
-      post.photo_id = runner.default_photo_id
+        minuteString = String.new
+        minutes = um.minutes
+        if minutes < 10
+          minuteString = "0" + minutes.to_s
+        else
+        minuteString = minutes.to_s
+        end
 
-      tempPosts.push(post)
-    end
+        message = "<a href=\"/profile/index?user_id=" + runner.id.to_s + "\">" + runner.fullname + "</a> ran the " + marathon.name + " and placed " + um.place.to_s +
+        " with a time of " + um.hours.to_s + ":" + minuteString
 
-    #get all the recently formed friendships
-    friendships = Friendship.where(:user_id => friendIDs)
-    friendships.each do |friendship|
+        post = Post.new
+        post.updated_at = um.updated_at
+        post.message = message
+        post.photo_id = runner.default_photo_id
 
-      friend = User.find(friendship.user_id )
-      friendOfFriend = User.find(friendship.friend_id )
+        tempPosts.push(post)
+      end
 
-      message = "<a href=\"/profile/index?user_id=" + friend.id.to_s + "\">" + friend.fullname + "</a> and <a href=\"/profile/index?user_id=" + friendOfFriend.id.to_s + "\">" + friendOfFriend.fullname + "</a> are now friends."
+      #get all the recently formed friendships
+      friendships = Friendship.where(:user_id => friendIDs)
+      friendships.each do |friendship|
 
-      #photo = Photo.find()
+        friend = User.find(friendship.user_id )
+        friendOfFriend = User.find(friendship.friend_id )
 
-      post = Post.new
+        message = "<a href=\"/profile/index?user_id=" + friend.id.to_s + "\">" + friend.fullname + "</a> and <a href=\"/profile/index?user_id=" + friendOfFriend.id.to_s + "\">" + friendOfFriend.fullname + "</a> are now friends."
 
-      post.updated_at = friendship.updated_at
-      post.message = message
-      post.photo_id = friendOfFriend.default_photo_id
-      post.user_id = friend.id
-      post.friend_id = friend.id
+        #photo = Photo.find()
 
-      tempPosts.push(post)
-    end
+        post = Post.new
 
-    #get all the recently added photos by friends
-    photos = Photo.where(:user_id => friendIDs)
-    photos.each do |photo|
+        post.updated_at = friendship.updated_at
+        post.message = message
+        post.photo_id = friendOfFriend.default_photo_id
+        post.user_id = friend.id
+        post.friend_id = friend.id
 
-      friend = User.find(photo.user_id )
-      message = "<a href=\"/profile/index?user_id=" + friend.id.to_s + "\">" + friend.fullname + "</a> uploaded a photo."
+        tempPosts.push(post)
+      end
 
-      post = Post.new
-      post.updated_at = photo.updated_at
-      post.message = message
-      post.photo_id = photo.id
+      #get all the recently added photos by friends
+      photos = Photo.where(:user_id => friendIDs)
+      photos.each do |photo|
 
-      tempPosts.push(post)
+        friend = User.find(photo.user_id )
+        message = "<a href=\"/profile/index?user_id=" + friend.id.to_s + "\">" + friend.fullname + "</a> uploaded a photo."
 
-      photocomments = photo.comments
-      photocomments.each do |comment|
-
-        if comment.user_id != friend.id
-
-          commenter = User.find(comment.user_id)
-
-          message = "<a href=\"/profile/index?user_id=" + commenter.id.to_s + "\">" + commenter.fullname + "</a> commented on <a href=\"/profile/index?user_id=" + friend.id.to_s + "\">" + friend.fullname +
-          "</a>'s photo.</br>" + comment.message
-
-          post = Post.new
-        post.updated_at = comment.updated_at
+        post = Post.new
+        post.updated_at = photo.updated_at
         post.message = message
         post.photo_id = photo.id
 
         tempPosts.push(post)
+
+        photocomments = photo.comments
+        photocomments.each do |comment|
+
+          if comment.user_id != friend.id
+
+            commenter = User.find(comment.user_id)
+
+            message = "<a href=\"/profile/index?user_id=" + commenter.id.to_s + "\">" + commenter.fullname + "</a> commented on <a href=\"/profile/index?user_id=" + friend.id.to_s + "\">" + friend.fullname +
+            "</a>'s photo.</br>" + comment.message
+
+            post = Post.new
+          post.updated_at = comment.updated_at
+          post.message = message
+          post.photo_id = photo.id
+
+          tempPosts.push(post)
+          end
         end
+
       end
 
+      #get all the comments of friends
+      comments = Comment.where(:user_id => friendIDs)
+      comments.each do |comment|
+
+        commenter = User.find(comment.user_id)
+        photo = Photo.find(comment.photo_id)
+        photosOwner = User.find(photo.id)
+
+        message = "<a href=\"/profile/index?user_id=" + commenter.id.to_s + "\">" + commenter.fullname + "</a> commented on <a href=\"/profile/index?user_id=" + photosOwner.id.to_s + "\">" + photosOwner.fullname + "</a>.</br>" + comment.message
+
+        post = Post.new
+        post.updated_at = comment.updated_at
+        post.message = message
+        post.photo_id = photo.id
+        post.user_id = commenter.id
+        post.friend_id = photosOwner.id
+
+        tempPosts.push(post)
+
+      end
+
+      tempPosts.sort_by{|e| e[:updated_at]}
+
+      sorted = tempPosts.reverse
+      @posts = sorted[0,49]
     end
-
-    #get all the comments of friends
-    comments = Comment.where(:user_id => friendIDs)
-    comments.each do |comment|
-
-      commenter = User.find(comment.user_id)
-      photo = Photo.find(comment.photo_id)
-      photosOwner = User.find(photo.id)
-
-      message = "<a href=\"/profile/index?user_id=" + commenter.id.to_s + "\">" + commenter.fullname + "</a> commented on <a href=\"/profile/index?user_id=" + photosOwner.id.to_s + "\">" + photosOwner.fullname + "</a>.</br>" + comment.message
-
-      post = Post.new
-      post.updated_at = comment.updated_at
-      post.message = message
-      post.photo_id = photo.id
-      post.user_id = commenter.id
-      post.friend_id = photosOwner.id
-
-      tempPosts.push(post)
-
-    end
-
-    tempPosts.sort_by{|e| e[:updated_at]}
-
-    sorted = tempPosts.reverse
-    @posts = sorted[0,49]
   end
 
   def add
