14923e6d57d4c50de211dcd1697dec9ca411dd9d
Manish Dangol <manishnm@hotmail.com>
2013-12-17 00:06:40 -0500

    fixes 1

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,6 @@ class Photo < ActiveRecord::Base
   has_attached_file :userphoto, :styles => {:thumb => "75x75", :small => "150x150>"}
   belongs_to :user
   belongs_to :marathon
-  has_many :comments
+  has_many :comments, :dependent=>:delete_all
   has_one :phototag
 end
